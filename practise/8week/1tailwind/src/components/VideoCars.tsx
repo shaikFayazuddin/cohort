068a2b3c0a171f79e77798ca2b1559350b0bbef9
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2024-03-09 11:00:02 +0530

    old commits

---
+++ practise/8week/1tailwind/src/components/VideoCars.tsx
@@ -0,0 +1,14 @@
+export function VideoCard(){
+    return <div>
+        <img src="/photo.png" alt="" />
+        <div className="grid gird-cols-12">
+            <div className="col-span-2">
+                <img src="/photo.png" alt="" />
+            </div>
+            <div className="col-span-10">
+                Tailwind : Demo(English)
+                Fayazuddin Shaik
+            </div>
+        </div>
+    </div>
+}
---
+++ practise/8week/2tailwind/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-between'>
+        <div className='bg-red-500'>Hello</div>
+        <div className='bg-red-500'>Hello</div>
+        <div className='bg-red-500'>Hello</div>
+      </div>
+      <div className='grid grid-cols-10'>
+        <div className='bg-yellow-200 col-span-5'>Bye</div>
+        <div className='bg-blue-100 col-span-3'>Bye</div>
+        <div className='bg-red-100 col-span-2'>Bye</div>
+      </div>
+      <div className='bg-red-700 sm:bg-red-500 md:bg-green-500'>Don't make it small</div>
+      <div className='grid grid-cols-9'>
+        <div className='bg-red-500 col-span-9 md:col-span-3'>Unity</div>
+        <div className='bg-blue-600 col-span-9 md:col-span-3'>and</div>
+        <div className='bg-blue-300 col-span-9 md:col-span-3'>Discipline</div>
+      </div>
+    </>
+  )
+}
+
+export default App
