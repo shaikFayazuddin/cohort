53186977b857eaacf9d8a7aa8ec441737d8ac8fd
shaikFayazuddin <shaikfayazuddin18@gmail.com>
2024-04-03 23:15:36 +0530

    typescript practise

---
+++ practise/9week/3typeScript/src/index.ts
@@ -0,0 +1,43 @@
+const x:number = 119
+console.log(x)
+
+////////////////
+function greet(firstName:any, lastName : string){
+    console.log("Hello " + firstName + " " + lastName)
+}
+
+greet("32","user")
+
+////////////////
+function sum(a:number, b:number) : number {
+    return a + b
+}
+
+const value = sum(56,34)
+console.log(value)
+
+/////////////
+function isLegal(age : number) : boolean{
+    if(age>18){
+        return true
+    }
+    return false
+}
+
+const answer = isLegal(28)
+console.log(answer)
+
+
+///////////////
+//create a funtion that takes another funtion as input and run after one second
+
+function mainFunction(argumentAsFunction : ()=>void){
+    setTimeout(argumentAsFunction,1000)
+}
+
+function afterOneSecond(){
+    console.log("this fucntion will be run after one second")
+}
+
+
+mainFunction(afterOneSecond)
