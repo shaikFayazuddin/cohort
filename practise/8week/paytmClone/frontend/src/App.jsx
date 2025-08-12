f2b61202d4d87f3a8c6d710d42f1e43bd225d791
Fayazuddin Shaik <shaikfayazuddin18@gmail.com>
2025-08-12 10:03:02 +0530

    changed landing page

--- practise/8week/paytmClone/frontend/src/App.jsx
+++ practise/8week/paytmClone/frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Signup/>} />
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
