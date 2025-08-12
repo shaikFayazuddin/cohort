01720fb003ddf6611735ca523b540698a4b836f5
Fayazuddin Shaik <shaikfayazuddin18@gmail.com>
2025-08-12 09:51:33 +0530

    changed url in users.js file

--- practise/8week/paytmClone/frontend/src/components/Users.jsx
+++ practise/8week/paytmClone/frontend/src/components/Users.jsx
@@ -11,16 +11,10 @@ export function Users({}){
   const [paytmUsers, setPaytmUsers] = useState([])
   const [filter, setFilter] = useState("")
 
-  // useEffect(()=>{
-  //   axios.get(`${DB_URL}/api/v1/user/bulk?filter=${filter}`).then(response=>setUsers(response.data.users))
-  // },[filter])
+  useEffect(()=>{
+    axios.get(`${DB_URL}/api/v1/user/bulk?filter=${filter}`).then(response=>setPaytmUsers(response.data.users))
+  },[filter])
 
-  useEffect(() => {
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
-            .then(response => {
-                setPaytmUsers(response.data.users)
-            })
-    }, [filter])
   console.log("the fetched users are", paytmUsers)
   return <div>
     <InputBox onChange={e=>setFilter(e.target.value)} inputLabel={"Users"} inputPlaceholder={"Search Users"} inputType={"text"}></InputBox>
